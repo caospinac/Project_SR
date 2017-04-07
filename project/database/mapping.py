b0e2b4bef938bcc11c1c6f757113768de6ec953d
Carlos A. Ospina <carlosaospinac@gmail.com>
2017-04-07 01:05:31 -0500

    First models

---
+++ project/database/mapping.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+    import sys
+    import os
+    sys.path.append(os.path.dirname(os.path.abspath(os.curdir)))
+
+from models import Admin
+from models import Card
+from models import Client
+from models import Crop
+from models import Fertilizer
+from models import Lab
+from models import Land
+from models import Lot
+from models import Nutrients
+from models import PriceFertilizer
+from models import Recomendation
+from models import User
+from base import DB
+
+
+DB.generate_mapping(create_tables=True)
