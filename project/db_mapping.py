97b1461aed9cf9e34bad2ed3c4e5518dc0353bf4
Carlos A. Ospina <carlosaospinac@gmail.com>
2017-04-09 20:55:36 -0500

    Delete unnecessary file

--- project/db_mapping.py
+++
@@ -1,8 +0,0 @@
-from models.base import engine
-from pony import orm
-
-
-orm.sql_debug(True)
-
-engine.bind("sqlite", "database.sqlite", create_db=True)
-engine.generate_mapping(create_tables=True)
