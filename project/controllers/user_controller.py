e9d2b685ef52af6fca7a8f334b210f99e969f3d2
Carlos A. Ospina <carlosaospinac@gmail.com>
2017-04-09 20:56:49 -0500

    Post in user full

--- project/controllers/user_controller.py
+++ project/controllers/user_controller.py
@@ -1,7 +1,34 @@
 from sanic.response import json
 from .base_controller import BaseController
+from pony.orm import db_session
+
+from models import User
 
 
 class UserController(BaseController):
     def get(self, request):
-        return json({'User': 'controller'})
+        return json({'User': 'controller', 'request': str(request.args)})
+
+    def post(self, request):
+        print(request.form)
+        req = request.form
+        with db_session:
+            try:
+                User(
+                    firstname=req.get('firstname'),
+                    lastname=req.get('lastname'),
+                    email=req.get('email'),
+                    phone=req.get('phone'),
+                    password=req.get('password'),
+                    lands=[],
+                )
+            except Exception as e:
+                print(e)
+                return super(UserController, self).post(request)
+            else:
+                return json({
+                    'success': {
+                        'code': 'TASK_SUCCESSFUL',
+                        'message': 'That task was successful',
+                    }
+                }, status=201)
