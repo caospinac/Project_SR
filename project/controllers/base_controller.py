d7ec933a81b395c3f1754c3c719a718f89ee2940
Carlos A. Ospina <carlosaospinac@gmail.com>
2017-04-10 23:16:42 -0500

    Basic CRUD for user added

--- project/controllers/base_controller.py
+++ project/controllers/base_controller.py
@@ -8,30 +8,30 @@ from http import HTTPStatus
 class BaseController(HTTPMethodView):
 
     @staticmethod
-    def response_status(code, message=None):
+    def response_status(code, data=None):
         try:
             status = dict(vars(HTTPStatus))['_value2member_map_'][code]
             return json({
                 'status': status.phrase,
                 'description': status.description,
                 'code': status.value,
-                'message': message
+                'data': data
             }, status=status.value)
         except KeyError as e:
             print(e)
             return json({})
 
     async def post(self, request, arg):
-        return self.__response_status(400)
+        return self.__response_status(501)
 
     async def get(self, request, arg):
-        return self.__response_status(404)
+        return self.__response_status(501)
 
     async def put(self, request, arg):
-        return self.__response_status(404)
+        return self.__response_status(501)
 
     async def patch(self, request, arg):
-        return self.__response_status(404)
+        return self.__response_status(501)
 
     async def delete(self, request, arg):
-        return self.response_status(404)
+        return self.response_status(501)
--- project/controllers/user_controller.py
+++ project/controllers/user_controller.py
@@ -1,14 +1,14 @@
 from sanic.response import json
 from .base_controller import BaseController
-from pony.orm import db_session
+from pony.orm import db_session, exists
 
 from models import User
 
 
 class UserController(BaseController):
     async def post(self, request):
-        print(request.form)
         req = request.form
+        print(request.form)
         with db_session:
             try:
                 User(
@@ -20,22 +20,45 @@ class UserController(BaseController):
                     lands=[],
                 )
             except Exception as e:
-                print(e)
                 return self.response_status(409)
         return self.response_status(201)
 
     async def get(self, request, id):
-        return json({'User': 'controller', 'request': str(request.args)})
+        with db_session:
+            if not exists(u for u in User if u.id == id):
+                return self.response_status(404)
+            return self.response_status(200, User[id])
 
     async def patch(self, request, id):
-        return self.response_status(200)
+        req = request.form
+        with db_session:
+            if not exists(u for u in User if u.id == id):
+                return self.response_status(404)
+            try:
+                User[id].set(
+                    firstname=req.get('firstname'),
+                    lastname=req.get('lastname'),
+                    email=req.get('email'),
+                    phone=req.get('phone'),
+                    password=req.get('password'),
+                    lands=[],
+                )
+            except Exception as e:
+                pass
+            else:
+                return self.response_status(200, User[id])
+        return self.response_status(204)
 
     async def delete(self, request, id):
-        if id:
-            with db_session:
-                try:
-                    User[id].delete()
-                except Exception as e:
-                    print(e)
-                    return self.response_status(404)
-        return self.response_status(200)
+        req = request.form
+        with db_session:
+            if not exists(u for u in User if u.id == id):
+                return self.response_status(404)
+            try:
+                User[id].set(
+                    active=False
+                )
+            except Exception as e:
+                return self.response_status(500)
+            else:
+                return self.response_status(200, User[id])
