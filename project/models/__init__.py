737a3d7a826b35409c7d55425b7c0ab83153c049
Carlos A. Ospina <carlosaospinac@gmail.com>
2017-04-11 18:07:27 -0500

    Resolve import

--- project/models/__init__.py
+++ project/models/__init__.py
@@ -5,7 +5,7 @@ from .fertilizer_price import FertilizerPrice
 from .lab import Lab
 from .land import Land
 from .lot import Lot
-from .nutrient_set import Nutrient_set
+from .nutrient_set import NutrientSet
 from .optimal_range_age import Optimal_range_age
 from .recomendation import Recomendation
 from .user import User
