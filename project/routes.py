395b12ad9213ef4c357b6271786690eed9975203
Carlos A. Ospina <carlosaospinac@gmail.com>
2017-04-19 23:05:55 -0500

    Add basic lot and land routes

--- project/routes.py
+++ project/routes.py
@@ -1,33 +1,63 @@
 from controllers import (
-    BaseController,
-    CardController,
-    CropController,
-    FertilizerController,
-    FertilizerPriceController,
-    LabController,
-    LandController,
-    LotController,
-    NutrientSetController,
-    OptimalRangeAgeController,
-    RecomendationController,
-    UserController
+    BaseController as Base,
+    CardController as Card,
+    CropController as Crop,
+    FertilizerController as Fertilizer,
+    FertilizerPriceController as FertilizerPrice,
+    LabController as Lab,
+    LandController as Land,
+    LotController as Lot,
+    NutrientSetController as NutrientSet,
+    OptimalRangeAgeController as OptimalRangeAge,
+    RecomendationController as Recomendation,
+    UserController as User
 )
 
 
 view_route_list = [
-    (BaseController.as_view(), '/base'),
-    (BaseController.as_view(), '/base/<arg>'),
-    (CardController.as_view(), '/card'),
-    (CropController.as_view(), '/crop'),
-    (FertilizerController.as_view(), '/fertilizer'),
-    (FertilizerPriceController.as_view(), '/fertilizer_price'),
-    (LabController.as_view(), '/lab'),
-    (LandController.as_view(), '/land'),
-    (LotController.as_view(), '/lot'),
-    (NutrientSetController.as_view(), '/nutrientset'),
-    (OptimalRangeAgeController.as_view(), '/optimal_range_age'),
-    (RecomendationController.as_view(), '/recomendation'),
-    (UserController.as_view(), '/user'),
-    (UserController.as_view(), '/user/<id:\w{32}>'),
-    (UserController.as_view(), '/user/<id:all>'),
+
+    # Base routes
+    (Base.as_view(), '/base'),
+
+    # Base routes
+    (Base.as_view(), '/base/<arg>'),
+
+    # Card routes
+    (Card.as_view(), '/card'),
+
+    # Crop routes
+    (Crop.as_view(), '/crop'),
+
+    # Fertilizer routes
+    (Fertilizer.as_view(), '/fertilizer'),
+
+    # FertilizerPrice routes
+    (FertilizerPrice.as_view(), '/fertilizer_price'),
+
+    # Lab routes
+    (Lab.as_view(), '/lab'),
+
+    # Land routes
+    (Land.as_view(), '/land'),
+    (Land.as_view(), '/land/<id:\w{32}>'),
+    (Land.as_view(), '/land/<id:all>'),
+
+    # Lot routes
+    (Lot.as_view(), '/lot'),
+    (Lot.as_view(), '/lot/<id:\w{32}>'),
+    (Lot.as_view(), '/lot/<id:all>'),
+
+    # NutrientSet routes
+    (NutrientSet.as_view(), '/nutrientset'),
+
+    # OptimalRangeAge routes
+    (OptimalRangeAge.as_view(), '/optimal_range_age'),
+
+    # Recomendation routes
+    (Recomendation.as_view(), '/recomendation'),
+
+    # User routes
+    (User.as_view(), '/user'),
+    (User.as_view(), '/user/<id:\w{32}>'),
+    (User.as_view(), '/user/<id:all>'),
 ]
