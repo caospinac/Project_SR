e06688d9a8241b4e28e7e3630a88c2f5d3f083e2
Carlos A. Ospina <carlosaospinac@gmail.com>
2017-04-20 12:49:15 -0500

    Fix SQL default for timestamp

--- agronify/models/crop.py
+++ agronify/models/crop.py
@@ -7,7 +7,7 @@ from .base import engine
 
 class Crop(engine.Entity):
     id = PrimaryKey(str, 32)
-    created = Required(datetime, sql_type='CURRENT_TIMESTAMP')
+    created = Required(datetime, sql_default='CURRENT_TIMESTAMP')
     modified = Required(datetime, sql_default='CURRENT_TIMESTAMP')
     active = Required(bool, default=True)
     name = Required(str, 40)
