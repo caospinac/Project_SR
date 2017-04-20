1a798c6541086488dce8acdbacaab55c609195ac
Carlos A. Ospina <carlosaospinac@gmail.com>
2017-04-20 13:52:40 -0500

    Add config

--- agronify/config.py
+++ agronify/config.py
@@ -1,26 +1,39 @@
-# //
+import urllib.parse as urlparse
+import os
+
+
 PRODUCTION = False
 
 if PRODUCTION:
-    import os
+    url = urlparse.urlparse(os.environ['DATABASE_URL'])
+    DB_USER = url.username
+    DB_PASSWORD = url.password
+    DB_HOST = url.hostname
+    DB_PORT = url.port
+    DB_NAME = url.path[1:]
     PORT = int(os.environ['PORT'])
 else:
+    DB_USER = 'postgres'
+    DB_PASSWORD = 'pgsql'
+    DB_HOST = 'localhost'
+    DB_PORT = 5432
+    DB_NAME = 'agroniify'
     PORT = 8000
 
 server = {
     'HOST': '0.0.0.0',
     'PORT': PORT,
-    'DEBUG': True,
+    'DEBUG': not PRODUCTION,
 }
 
 database = {
     'DB_CLIENT': 'sqlite',
     'DB_TEST_CLIENT': 'sqlite',
-    'DB_USER': 'postgres',
-    'DB_PASSWORD': 'pgsql',
-    'DB_PORT': 5432,
-    'DB_HOST': 'localhost',
-    'DB_NAME': 'agroniify',
+    'DB_USER': DB_USER,
+    'DB_PASSWORD': DB_PASSWORD,
+    'DB_HOST': DB_HOST,
+    'DB_PORT': DB_PORT,
+    'DB_NAME': DB_NAME,
     'DB_TEST_NAME': 'test.sqlite',
     'SQL_DEBUG': not PRODUCTION,
 }
