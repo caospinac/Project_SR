eb4fc58fd225c669deb6467770cf7ca5503750e9
Carlos A. Ospina <carlosaospinac@gmail.com>
2017-04-22 22:23:15 -0500

    Remove Lot Entity

--- agronify/controllers/ideal_controller.py
+++ agronify/controllers/ideal_controller.py
@@ -2,6 +2,6 @@ from sanic.response import json
 from .base_controller import BaseController
 
 
-class OptimalRangeAgeController(BaseController):
+class IdealController(BaseController):
     async def get(self, request, arg):
         return json({'OptimalRangeAge': 'controller'})
